237067029f0dfc0c5941aee44cc8b6d3998b9e6f
Kris Natseway <knatseway@cnm.edu>
2020-11-01 19:51:54 -0700

    Try on a setup with a tutorial.  Webstorm throwing errors.

---
+++ user.js
@@ -0,0 +1,16 @@
+
+
+
+
+e.preventDefault();
+this.setState({
+    aboutYourself:this.aboutYourself.current.value
+})
+}
+
+render(){
+    return(
+        <div className="app">
+            <h1>This is
+    )
+}
